04f6d6f0e024ba8084f6b43bc80ab7d904f78a1d
Markus Becker <mtb@tibyte.net>
2016-01-15 13:52:12 +0100

    init layout

---
+++ index.php
@@ -0,0 +1,29 @@
+<?
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Freie Schülerzeitung</title>
+        <link rel="stylesheet" href="web/css/main.css" charset="utf-8">
+    </head>
+    <body>
+        <div id="header">
+            Freie Schülerzeitung
+        </div>
+        <div id="navigator">
+            <a href="#">Blog</a><a href="#">Über Uns</a><a href="#">Impressum</a>
+        </div>
+        <div id="container">
+            <h2>Eintrag 1</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <h2>Eintrag 2</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+    </body>
+</html>
